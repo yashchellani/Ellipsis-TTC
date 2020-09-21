a8cc566db2dbb0b449bdad6c7d8db40d8a0cee8e
Yash Chellani <35882565+yashchellani@users.noreply.github.com>
2020-09-21 19:59:56 +0530

    Create almostsorted-attempt1.py

---
+++ Challenge 1/almostsorted-attempt1.py
@@ -0,0 +1,23 @@
+def solve(arr):
+    #proxy for stdin parameter
+    arr_count = len(arr) 
+    #if arr length is 0 or 1, the only possible subsequence is itself. returns array length+1 if array is already sorted.
+    if len(arr) <= 1:
+        return arr_count
+    #initialising totalIntervals
+    totalIntervals = arr_count
+    for i in range(arr_count):
+        tempList = [arr[i]]
+        #print(tempList)
+        for j in range (i+1,arr_count):
+            if arr[j] >= tempList[-1]:
+                totalIntervals += 1
+                tempList.append(arr[j])
+                print(tempList)
+            else:
+                break
+        tempList = []
+        
+    return totalIntervals
+
+print(solve([1,2,2]))
