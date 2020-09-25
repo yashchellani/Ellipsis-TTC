b7134194b034135beec3c5dd6aebc795d238ab0e
yunsoe <39767972+yunsoe@users.noreply.github.com>
2020-09-25 08:46:56 +0800

    Create Popping_blocks.py

---
+++ Challenge 1/Popping_blocks.py
@@ -0,0 +1,43 @@
+a = ['B', 'B', 'B', 'A', 'C', 'A', 'A', 'C']
+b = [1, 1, 1, 1, 1]
+
+
+def check_all_equal(array):
+    count = 0
+    num = array[0]
+    for i in range(1, len(array)):
+        if array[i] != num:
+            count += 1
+
+    return count
+
+def popping2(array):
+    if len(array) <= 1:
+        print(array)
+        return
+
+    if len(array) % 2 == 0:
+        result = check_all_equal(array)
+        if result == 0:
+            print([])
+            return
+
+    i = 1
+    while i < len(array) - 1 :
+        if array[i] == array[i - 1]:
+            array = array[:i-1] + array[i+1:]
+            i = 1
+        elif array[i] == array[i + 1]:
+            array = array[:i] + array[i+2:]
+            i = 1
+        else:
+            i += 1
+            
+        #print(len(array))
+        #print(i)
+        #print(array)
+
+    print(array)
+   
+print(popping2(a))
+   
